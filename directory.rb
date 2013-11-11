a0468b98935b057b5a65a967a244740c5920cac4
Nisar Tahir <nisar.tahir@gmail.com>
2013-11-11 13:30:27 +0000

    Added cohort information

--- directory.rb
+++ directory.rb
@@ -2,7 +2,14 @@
 #Lets put the students into an array
 
 students = []
-students = ['Michael', 'Jack', 'Jeremy', 'Bruce', 'Nisar', 'James']
+students = [
+  ['Michael', :november],
+  ['Jack',  :november],
+  ['Jeremy', :november],
+  ['Bruce', :november],
+  ['Nisar', :november],
+  ['James', :november]
+]
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -11,8 +18,8 @@ end
 
 def print_students(names)
 	#now we print a list of the students
-	names.each do |name|
-		puts name
+	names.each do |name, cohort|
+		puts "#{name} is part of the #{cohort} cohort"
 	end
 end
 
