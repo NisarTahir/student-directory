0e755a4138efc10288b5f67fca3e37852031696f
Nisar Tahir <nisar.tahir@gmail.com>
2013-11-11 14:21:45 +0000

    Asking for names of students

--- directory.rb
+++ directory.rb
@@ -1,14 +1,14 @@
 
 #Lets put the students into an array
 
-students = []
+students = {}
 students = [
-  ['Michael', :november],
-  ['Jack',  :november],
-  ['Jeremy', :november],
-  ['Bruce', :november],
-  ['Nisar', :november],
-  ['James', :november]
+  {:name => 'Michael', :cohort => :november},
+  {:name => 'Jack',  :cohort => :november},
+  {:name => 'Jeremy', :cohort => :november},
+  {:name => 'Bruce', :cohort => :november},
+  {:name => 'Nisar', :cohort => :november},
+  {:name => 'James', :cohort => :november}
 ]
 
 def print_header
@@ -18,17 +18,30 @@ end
 
 def print_students(names)
 	#now we print a list of the students
-	names.each do |name, cohort|
-		puts "#{name} is part of the #{cohort} cohort"
+	names.each do |student|
+		puts "#{student[:name]} is part of the #{student[:cohort]} cohort"
 	end
 end
 
 def print_footer(names)
 	#finally, we print the total
-	print "Overall, we have #{names.length} great students"
+	puts "Overall, we have #{names.length} great students"
 end
 
+puts "Enter a student name and then press ENTER\nWhen you have finished type END and press ENTER"
+
+student_names =[]
+
+until student_names.last == "END"
+  student_names << gets.chomp
+end
 
 print_header
-print_students(students)
-print_footer(students)
+student_names.pop
+puts student_names
+print_footer(student_names)
+
+
+#print_header
+#print_students(students)
+#print_footer(students)
