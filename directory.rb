09b5333e4c852014e0d1eccfa08dd15214821b27
Nisar Tahir <nisar.tahir@gmail.com>
2013-11-11 13:19:00 +0000

    Created methods

--- directory.rb
+++ directory.rb
@@ -1,13 +1,27 @@
+
 #Lets put the students into an array
 
 students = []
-students = [ 'Michael', 'Jack', 'Jeremy', 'Bruce', 'Nisar', 'James']
+students = ['Michael', 'Jack', 'Jeremy', 'Bruce', 'Nisar', 'James']
 
-#now we print a list of the students
-puts "The students of my cohort at Makers Academy"
-puts "-------------------------------------------"
-students.each do |student|
-	puts student
+def print_header
+	puts "The students of my cohort at Makers Academy"
+	puts "-------------------------------------------"
 end
-#finally, we print the total
-print "Overall, we have #{students.length} great students"
+
+def print_students(names)
+	#now we print a list of the students
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+	#finally, we print the total
+	print "Overall, we have #{names.length} great students"
+end
+
+
+print_header
+print_students(students)
+print_footer(students)
